351a9d5e40ea9e81317fd5e792cec6ebd9965207
Prashant Vijay Patil <89566694+Prashant-PSys@users.noreply.github.com>
2021-10-20 08:38:36 +0530

    Delete src directory

--- src/main/java/com/perennial/sme_payroll/dao/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.perennial.sme_payroll.dao;
-
-import com.perennial.sme_payroll.entity.Employee;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends CrudRepository<Employee,Integer> {
-}
